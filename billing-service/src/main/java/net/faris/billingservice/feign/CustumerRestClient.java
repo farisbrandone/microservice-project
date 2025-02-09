6f4bbc458e82fe3e0464809b566c9a17a23f5536
farisbrandone <farisbrandone@yahoo.com>
2025-02-09 20:34:15 +0100

    v3

--- billing-service/src/main/java/net/faris/billingservice/feign/CustumerRestClient.java
+++ billing-service/src/main/java/net/faris/billingservice/feign/CustumerRestClient.java
@@ -1,7 +1,9 @@
 package net.faris.billingservice.feign;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import net.faris.billingservice.model.Custumer;
 import net.faris.billingservice.model.Product;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.hateoas.PagedModel;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,8 +12,18 @@ import org.springframework.web.bind.annotation.PathVariable;
 @FeignClient(name = "custumer-service")
 public interface CustumerRestClient {
     @GetMapping("/api/custumers/{id}")
-     Custumer findCustumerById(@PathVariable Long id);
+    @CircuitBreaker(name = "custumerServiceCB", fallbackMethod = "getDefaultCustumer")
+    Custumer findCustumerById(@PathVariable Long id);
 
     @GetMapping("/api/custumers")
+    @CircuitBreaker(name = "custumerServiceCB", fallbackMethod = "getDefaultCustumers")
     PagedModel<Custumer> getAllCustumers();
+
+    default Custumer getDefaultCustumer(Long id, Exception e) {
+        return new Custumer(id, "Default Name", "Default Email");
+    }
+
+    default PagedModel<Custumer>  getDefaultCustumers(Exception e) {
+        return PagedModel.empty();
+    }
 }
--- billing-service/src/main/java/net/faris/billingservice/model/Custumer.java
+++ billing-service/src/main/java/net/faris/billingservice/model/Custumer.java
@@ -5,6 +5,13 @@ public class Custumer {
     private String name;
     private String email;
 
+
+    public Custumer(Long id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
     public Long getId() {
         return id;
     }
